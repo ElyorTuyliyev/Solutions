facb44421cdb17256d1a4f6eb155f0eabca113e3
ElyorTuyliyev <tuyliyevelyor@gmail.com>
2025-12-24 15:45:24 +0500

    fix task

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 // const arr = [1,'s',23,'yy',-99,0]
-
 // function numberFilter(l) {
 //     return l.filter((val)=>Number.isFinite(val))
 // }
-
-
 // console.log(numberFilter(arr));
 
 
 // const number = -1
-
 // const ss = function (num) {
 //     const res = Math.sqrt(number)
 //     if(Number.isInteger(res)) {
@@ -18,19 +14,36 @@
 //         return false
 //     }
 // }
-
 // console.log(ss());
 
 
 // const arr = ["Bolta","Altynai","Adilet","Aibek","Kubas","Aibolat"]
-
 // function filterByA(l) {
 //     return l.filter((val)=> val.length === 4)
 // }
-
 // console.log(filterByA(arr));
 
 
 // const numbers = 1947837
 // const res = numbers.toString().split("").reverse().map(Number)
 // console.log(res);
+
+
+// function hoopCount (n) {
+//    if (n<10) {
+//      return "Keep at it until you get it"
+//    } else {
+//       return "Great, now move on to tricks"
+//    }
+// } 
+
+
+// function updateLight(current) {
+//   if  (current==="green") {
+//      return "yellow"
+//   } else if (current==="yellow") {
+//   return "red"
+//    } else if (current==="red") {
+// return "green"
+//    }
+// }
