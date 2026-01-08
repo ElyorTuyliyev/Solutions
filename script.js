88c403141aa1083f495a9bc16f9b7e6019d1f925
ElyorTuyliyev <tuyliyevelyor@gmail.com>
2026-01-08 10:17:18 +0500

    fix task

--- script.js
+++ script.js
@@ -183,16 +183,16 @@
 // }
 // console.log(oppositeNumber(-1.22));
 
- const arr = [true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true, undefined ,null  ]
+//  const arr = [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true, undefined ,null  ]
 
 
-  function countSheep(sheep) {
-   return arr.filter((value)=>value===true).length
-}
+//   function countSheep(sheep) {
+//    return arr.filter((value)=>value===true).length
+// }
 
-console.log(countSheep());
+// console.log(countSheep());
